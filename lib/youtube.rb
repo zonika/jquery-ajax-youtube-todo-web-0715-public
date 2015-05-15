0d1a2318843b886a31147d889e3f8da7eec8608f
changamanda <amanda.chang10@gmail.com>
2015-05-15 14:40:38 -0400

    Use the YT gem

--- lib/youtube.rb
+++ lib/youtube.rb
@@ -1,12 +1,15 @@
 class YouTube
 
-  RETURN_TO_SENDER = '<iframe width="640" height="390" src="//www.youtube.com/embed/PU5xxh5UX4U" frameborder="0" allowfullscreen></iframe>'
-  
-  attr_reader :client, :search_term
+  RETURN_TO_SENDER = '<iframe id="ytplayer" width="640" height="390" src="//www.youtube.com/embed/PU5xxh5UX4U" frameborder="0" allowfullscreen></iframe>'
+
+  attr_reader :search_term, :videos
 
   def initialize(search_term)
-    @client = YouTubeIt::Client.new(:dev_key => ENV['DEVELOPER_KEY'])
+    Yt.configure do |config|
+      config.api_key = ENV['DEVELOPER_KEY']
+    end
     @search_term = search_term
+    @videos = Yt::Collections::Videos.new
   end
 
   def self.run(search_term)
@@ -14,12 +17,10 @@ class YouTube
   end
 
   def get_vid
-    videos = self.client.videos_by(:query => [self.search_term]).videos
-    videos.empty? ? RETURN_TO_SENDER : get_embedded_vid(videos)
-  end
-
-  def get_embedded_vid(videos)
-    videos.first.embed_html5(:id => "ytplayer", :width => 640, :height => 390)
+    video = self.videos.where(q: self.search_term, order: 'viewCount').first
+    video_url = video.embed_html
+    video_url.gsub!("<iframe",'<iframe id="ytplayer"')
+    video.empty? ? RETURN_TO_SENDER : video.embed_html
   end
 
 end
